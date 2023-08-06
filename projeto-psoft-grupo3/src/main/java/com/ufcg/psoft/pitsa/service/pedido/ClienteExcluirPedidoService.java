fab1e40f8fb829af9ae621dfaa847597bc0cb8e9
Catarina Ramalho <50886615+catarinaramalho@users.noreply.github.com>
2023-08-05 22:23:26 -0300

    Delete projeto-psoft-grupo3/src/main/java/com/ufcg/psoft/pitsa/service/pedido directory

--- projeto-psoft-grupo3/src/main/java/com/ufcg/psoft/pitsa/service/pedido/ClienteExcluirPedidoService.java
+++
@@ -1,6 +0,0 @@
-package com.ufcg.psoft.pitsa.service.pedido;
-
-@FunctionalInterface
-public interface ClienteExcluirPedidoService {
-    void clienteExcluir(Long pedidoId, Long clienteExclusorId);
-}
--- projeto-psoft-grupo3/src/main/java/com/ufcg/psoft/pitsa/service/pedido/PedidoConfirmarPagamentoService.java
+++
@@ -1,9 +0,0 @@
-package com.ufcg.psoft.pitsa.service.pedido;
-
-import com.ufcg.psoft.pitsa.dto.PedidoPostPutRequestDTO;
-import com.ufcg.psoft.pitsa.model.Pedido;
-
-@FunctionalInterface
-public interface PedidoConfirmarPagamentoService {
-    Pedido confirmarPagamento(Long clienteId, Long pedidoId, String metodoPagamento, String codigoAcessoCliente, PedidoPostPutRequestDTO pedidoPostPutRequestDTO);
-}
